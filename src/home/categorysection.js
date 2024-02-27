f6c8057aaf218d3cbd3ebcb55e44a406f0829067
Keerthi <keerthana14298@gmail.com>
2024-02-27 19:08:08 +0530

    first update

---
+++ src/home/categorysection.js
@@ -0,0 +1,16 @@
+function categorySection(){
+    return(
+        <>
+        <section className="mainbox">
+            <div className="row">
+                <div className="col-md-6 catbox"></div>
+                <div className="col-md-6">
+                    <div className="row">
+                        <div className="col-md-12"></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        </>
+    )
+}
